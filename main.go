cdd0a6c62cd2211ea814a58bcf2d6a6bcdc55991
GSM1900\SJain28 <sumit.jain28@t-mobile.com>
2018-08-12 18:32:20 -0400

    added one more endpoint

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
+	Id      string `json:"Id"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
@@ -16,14 +19,31 @@ type Article struct {
 
 type Articles []Article
 
+var articles Articles
+
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
+
+	//articles := Articles{
+	//	Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+	//	Article{Id: "2", Title: "Hello 2", Desc: "Article Description2", Content: "Article Content2"},
+	//}
 	fmt.Println("Endpoint Hit: returnAllArticles")
 
 	json.NewEncoder(w).Encode(articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	//fmt.Fprintf(w, "ID: "+id)
+	for _, item := range articles {
+		if item.Id == id {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	fmt.Println("Endpoint Hit: returnSingleArticle")
+	json.NewEncoder(w).Encode(&Article{})
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -37,12 +57,15 @@ func handleRequests() {
 		port = fromEnv
 	}
 
-	server := http.NewServeMux()
+	server := mux.NewRouter().StrictSlash(true)
 	server.HandleFunc("/", homePage)
-	server.HandleFunc("/all", returnAllArticles)
+	server.HandleFunc("/articles", returnAllArticles)
+	server.HandleFunc("/article/{id}", returnSingleArticle)
 	log.Fatal(http.ListenAndServe(":"+port, server))
 }
 
 func main() {
+	articles = append(articles, Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"})
+	articles = append(articles, Article{Id: "2", Title: "Hello 2", Desc: "Article Description2", Content: "Article Content2"})
 	handleRequests()
 }
